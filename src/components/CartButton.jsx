fd079fd72c8367249b72262885ffaf9a1c487250
Nguyễn Đức Anh <anhnd1223@gmail.com>
2025-04-04 12:10:05 +0700

    Added some interaction

--- src/components/CartButton.jsx
+++ src/components/CartButton.jsx
@@ -1,31 +1,54 @@
-const CartButton = ({ items }) => {
+import { useEffect, useState } from "react"
+
+const CartButton = ({ cart }) => {
+    const [isDropdownVisible, setIsDropdownVisible] = useState(false);
+    const toggleDropdown = () => {
+        setIsDropdownVisible((prevState) => {
+            return !prevState;
+        });
+      };
+
     return (
         <div style={{ display: "flex", justifyContent: "flex-end" }}>
             <div style={{ position: "relative", marginBottom: "20px" }}>
-                <button>
+                <button onClick={toggleDropdown}>
                     Giỏ hàng
                 </button>
-                <div className='notification'>
-                    3
-                </div>
-                <ul className="dropdown-list">
-                    {items.map((item) => (
-                        <ItemInCart key={item.id} item={item} />
-                    ))}
-                </ul>
+                <Notification cart={cart}/>
+                {cart && isDropdownVisible && (
+                    <ul className="dropdown-list">
+                        {cart.map((cartItem) => (
+                            <ItemInCart key={cartItem.id} item={cartItem} />
+                        ))}
+                    </ul>
+                )}
             </div>
         </div>
     )
 }
 
+const Notification = ({ cart }) => {
+    const totalQuantity = cart.reduce((total, product) => total + product.quantity, 0);
+  
+    if (totalQuantity === 0) {
+      return null;
+    }
+  
+    return (
+      <div className='notification'>
+        {totalQuantity}
+      </div>
+    );
+  };
+
 const ItemInCart = ({ item }) => {
     return (
         <li>
-            <div className="row-minicontainer" style={{gap:"10px"}}>
+            <div className="row-minicontainer" style={{ gap: "10px" }}>
                 <img src={item.image} alt={item.name} title={item.name} width={100} height={100} />
                 <div className="column-minicontainer">
                     <div className="item-title">{item.name}</div>
-                    <div className="item-text">{item.price} VND</div>
+                    <div className="item-text"> Thành tiền: {item.price.toLocaleString()} x {item.quantity} = {(item.price*item.quantity).toLocaleString()} VND</div>
                 </div>
             </div>
         </li>
--- src/components/ItemCard.jsx
+++ src/components/ItemCard.jsx
@@ -1,15 +1,15 @@
 
-const ItemCard = ({ item }) => {
+const ItemCard = ({ item, onclick }) => {
     return (
         <div className="item-card-container" key={item.id}>
             <img src={item.image} alt={item.name} title={item.name} width={300} height={300} />
             <div className="row-minicontainer" style={{justifyContent:"space-between",alignContent:"center"}}>
                 <div className="column-minicontainer">
                     <div className="item-title">{item.name}</div>
-                    <div className="item-text">{item.price} VND</div>
+                    <div className="item-text">{item.price.toLocaleString()} VND</div>
                 </div>
                 <div>
-                    <button> Mua </button>
+                    <button onClick={() => onclick(item.id,1)}> Mua </button>
                 </div>
             </div>
         </div>
